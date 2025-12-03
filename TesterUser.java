1a5850f87680e4442c075cc39ac21b93d6b2a544
Zeren <zerencoban07@gmail.com>
2025-12-03 19:36:21 +0300

    added comment by idil-zeren

--- TesterUser.java
+++ TesterUser.java
@@ -1,6 +1,23 @@
 // File: TesterUser.java
+
+/**
+ * Represents a user with the {@link Role#TESTER} role.
+ * <p>
+ * Testers have read-only access to the system (list, search, sort)
+ * and cannot modify contact information.
+ * </p>
+ */
 public class TesterUser extends User
 {
+    /**
+     * Creates a new TesterUser with the specified account information.
+     *
+     * @param userId       unique identifier of the user
+     * @param username     login username
+     * @param passwordHash hashed password
+     * @param name         first name of the user
+     * @param surname      last name of the user
+     */
     public TesterUser(int userId,
                       String username,
                       String passwordHash,
@@ -10,6 +27,10 @@ public class TesterUser extends User
         super(userId, username, passwordHash, name, surname, Role.TESTER);
     }
 
+    /**
+     * Opens the menu interface for the tester user.
+     * Delegates control to {@link TesterMenu#start()}.
+     */
     @Override
     public void openMenu()
     {
